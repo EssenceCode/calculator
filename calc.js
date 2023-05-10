75701e82628db91389a5c21a252cb5769cbc0c4c
EssenceCode <123456789+odin@users.noreply.github.com>
2023-05-10 10:35:22 +0800

    calculator bug fixing
    updated the doMath function so you can string multiple operators
    edited result so it will not display answers with long decimals
    edited getResult so you cant divide by 0

--- calc.js
+++ calc.js
@@ -5,19 +5,19 @@ const operatorBtn = document.querySelectorAll('.operator');
 const acBtn = document.querySelector('.all-clear');
 const delBtn = document.querySelector('.delete');
 const equalsBtn = document.querySelector('.equals');
-const decimal = document.querySelector('.decimal');
+// const decimal = document.querySelector('.decimal');
 // event listener
 numBtn.forEach(number => number.addEventListener('click', (e) => {
-    populate(e)
+    populate(e),
     getValue();
 }));
 operatorBtn.forEach(operator => operator.addEventListener('click', (e) => {
-    displayOperator(e),
-    doMath() 
+    getOperator(e),
+    doMath(e) 
 }))
 acBtn.addEventListener('click', allClear);
 equalsBtn.addEventListener('click', getResult);
-decimal.addEventListener('click',populate);
+
 
 // create a function of basic math operators
 function add(x, y) {
@@ -37,7 +37,7 @@ function divide(x, y) {
 };
 
 let num1 = null;
-let operator = null;
+let operator1 = null;
 let operator2 =null;
 let num2 = null;
 let result = '';
@@ -45,7 +45,7 @@ let result = '';
 function operate(x,operators,y) {
     x = Number(x);
     y = Number(y);
-    console.log(`num1:${x} math:${operator} num2: ${y}`)
+    
     if (operators === '+') {return add(x,y)}
     else if (operators === '-') { return subtract(x,y)}
     else if (operators === '*') {return multiply(x,y)}
@@ -56,88 +56,96 @@ function operate(x,operators,y) {
 // create a function that will display the num values
 function populate(e) {
     let value = e.target.textContent;
-    if (display.textContent === '0') { display.textContent = ''}
-    else if (operator !== null && num2 === null) { display.textContent = ''}
-    else if (num1 === result && num2 === null) { display.textContent = ''}
+    if (display.textContent === '0') { 
+        display.textContent = ''
+    }
+    else if (operator1 !== null && num2 === null) { 
+        display.textContent = ''
+    }
+    else if (num1 === result && num2 === null) { 
+        display.textContent = ''
+    }
     return display.textContent += value;
 };
 
 
 // write a function that will store the number to the variables
 function getValue() {
-    if (operator !== null || operator2 !== null) {
+    if (operator1 !== null || operator2 !== null) {
         num2 = display.textContent;
         
     }
-    else if (operator === null) {
+    else if (operator1 === null) {
         num1 = display.textContent;
-        // console.log(`${num1}`)
+       
     }
 }
-function displayOperator(e) {
-    if (num1 !== null && num2 === null ) {
-        operator = e.target.textContent
-        // display.textContent = ''
+function getOperator(e) {
+    if (num1 !== null && num2 === null) {
+        operator1 = e.target.textContent
     }
-    else if (operator !== null && operator2 === null) {
+    else if (operator1 !== null && operator2 === null) {
         operator2 = e.target.textContent
     }
+    // else if (operator2 !== null && num1 === result && num2 !== null) {
+    //     operator1 = e.target.textContent
+    // }
 };
 
-function doMath() {
-    if (num1 !== null && operator !== null && num2 !== null) {
-        result = operate(num1,operator,num2);
-        console.log(operator);
-        num1 = result;
+function doMath(e) {
+    if (num1 !== null && operator1 !== null && num2 !== null) {
+        result = operate(num1,operator1,num2);
+        num1 = Number(result.toFixed(2));
         num2 = null;
-        operator = null;
+        operator1 = null;
         display.textContent = num1;
-        console.log(result)
+        
     }
     else if (num1 === result && operator2 !== null && num2 !== null) {
+        operator1 = e.target.textContent
         result = operate(num1,operator2,num2);
-        num1 = result;
+        num1 = Number(result.toFixed(2));
         operator2 = null;
-        display.textContent = result;
         num2 = null;
+        display.textContent = num1;
     }
 }
 
 // write a function that will calculate the two number variables
 function getResult(e) {
-    if (operator !== null) {
-        result = operate(num1,operator,num2);
-        num1 = result;
+    if (num1 === '0' || num2 === '0' && operator1 === '/') {
+       return display.textContent = 'ERROR'
+   }
+   
+   else if (num1 === 0 && operator2 === '/') {
+       return display.textContent = 'ERROR'
+  }
+
+    
+    if (operator1 !== null) {
+        result = operate(num1,operator1,num2)
+        num1 = Number(result.toFixed(2));
         num2 = null;
-        operator = null;
+        operator1 = null;
         operator2 = null;
-        display.textContent = result;
+        return display.textContent = num1;
     }
     else if (operator2 !== null) {
         result = operate(num1,operator2,num2);
-        num1 = result;
+        num1 = Number(result.toFixed(1));
         num2 = null;
-        operator = null;
+        operator1 = null;
         operator2 = null;
-        display.textContent = result;
+        return display.textContent = num1;
 
     }
-    if (num1 === '0' && operator === '/') {
-         display.textContent = 'ERROR'
-    }
-    else if (num2 === '0' && operator === '/') {
-         display.textContent = 'ERROR'
-    }
-    else if (num1 === 0 && operator === '/') {
-        display.textContent = 'ERROR'
-   }
-
+   
 }
 
 function allClear(e) {
     num1 = null;
     num2 = null;
-    operator = null;
+    operator1 = null;
     operator2 = null;
     result = null;
     return display.textContent = '0';
